58cd125352e53910f9f8edce5b5fb057b7d3d47e
AMIT SINGH <99306249+as47-chauhan@users.noreply.github.com>
2022-03-29 19:45:44 +0530

    Day-01 Assingments
    Answers - e, b, f, i.

---
+++ Amit Singh/Day-01/Number_Pattern.java
@@ -0,0 +1,25 @@
+package patterns;
+
+public class Number_Pattern {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+	       for(int i=1; i<=n; i++) {
+	           //spaces
+	           for(int j=1; j<=n-i; j++) {
+	               System.out.print("   ");
+	           }
+	 
+	           //first part
+	           for(int j=i; j>=1; j--) {
+	               System.out.print(" "+(i-1)+" ");
+	           }
+	 
+	           //second part
+	           for(int j=2; j<=i; j++) {
+	               System.out.print(" "+(i-1)+" ");
+	           }
+	           System.out.println();
+	       }
+	}
+}
