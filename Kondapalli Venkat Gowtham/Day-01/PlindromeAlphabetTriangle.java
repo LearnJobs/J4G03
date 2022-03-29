18c7cc4b53692b638447060f7fb85a6fb06e99a4
Gowtham-KV <97107560+Gowtham-KV@users.noreply.github.com>
2022-03-29 22:09:48 +0530

    Add files via upload

---
+++ Kondapalli Venkat Gowtham/Day-01/PlindromeAlphabetTriangle.java
@@ -0,0 +1,19 @@
+package begin;
+
+public class PlindromeAlphabetTriangle {
+
+	public static void main(String[] args) {
+		int spaces =20;
+		int letter =1;
+		int alphabet = 97; //ASCII value of a is 97
+		for(int i=0;i<spaces;i++) {
+			for(int j=0;j<spaces;j++)System.out.print(" ");
+			for(int j=0;j<letter;j++)System.out.print((char)(alphabet+j));
+			for(int j=letter-2;j>=0;j--)System.out.print((char)(alphabet+j));
+			System.out.println();
+			spaces--;
+			letter++;
+			
+		}//System.out.print((char)(alphabet+i));
+		
+}}
