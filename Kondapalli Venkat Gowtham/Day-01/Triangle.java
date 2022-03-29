08bf1040dc2db3a6ad9b60f72a48ba4d5eec1b1d
Gowtham-KV <97107560+Gowtham-KV@users.noreply.github.com>
2022-03-29 16:04:47 +0530

    Add files via upload

---
+++ Kondapalli Venkat Gowtham/Day-01/Triangle.java
@@ -0,0 +1,18 @@
+package begin;
+
+public class Triangle {
+	public static void main(String[] args) {
+		int spaces =17;
+		int star = 1;
+	 for(int i=0;i<17;i++){
+		 System.out.print(" ");
+		 for(int j =0;j<spaces;j++)
+			 System.out.print(" ");		 
+		 for(int j =0;j<star;j++) 
+			 System.out.print("*"); 
+		 System.out.println(); 
+		 spaces -=1;
+		 star+=2;
+	 } 
+	}
+}
