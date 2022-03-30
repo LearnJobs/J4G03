90eba3af47422ec16f185eee4e513a957ab4b90d
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-03-30 14:48:42 +0530

    Add files via upload

---
+++ Bolloju Satya Akil Kumar/day-01/Figure_g.java
@@ -0,0 +1,27 @@
+package begin;
+
+public class Figure_g {
+
+	public static void main(String[] args) {
+		int spaces = 14;
+		
+		for(int i=1;i<=9;i++) {
+			for(int j=1;j<=i;j++)System.out.print(j);
+			for(int j=0;j<=spaces;j++)System.out.print(" ");
+			for(int j=i;j>0;j--) {
+				if(j!=9) {
+					System.out.print(j);
+				}
+				else if(j==9) {
+					j=8;
+					System.out.print(j);
+				}
+			}
+			spaces-=2;
+			System.out.println();
+		}
+			
+
+	}
+
+}
