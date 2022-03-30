755775c57d3f5c5524942063cfb7ec06f23c584a
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-03-30 12:12:32 +0530

    Add files via upload

---
+++ Bolloju Satya Akil Kumar/day-01/PalindromeAlphabetTriangle.java
@@ -0,0 +1,17 @@
+package begin;
+
+public class PalindromeAlphabetTriangle {
+
+	public static void main(String[] args) {
+		int spaces =20;
+		
+		for(int i =1;i<=10;i++) {
+			int alphabet=64;
+			for(int j=1;j<spaces;j++)System.out.print(" ");
+			for(int j=1;j<=i;j++) System.out.print((char) (alphabet +j));
+			for(int j=i-1;j>0;j--)System.out.print((char) (alphabet +j));
+			System.out.println();
+			spaces--;	
+		}
+	}
+}
