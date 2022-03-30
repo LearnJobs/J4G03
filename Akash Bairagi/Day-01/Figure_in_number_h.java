f20e1800b2f46808b8be87a156678f00c4f2c8c6
akash-0504 <58282423+akash-0504@users.noreply.github.com>
2022-03-30 20:54:19 +0530

    Add files via upload

---
+++ Akash Bairagi/Day-01/Figure_in_number_h.java
@@ -0,0 +1,31 @@
+
+public class Figure_in_number_h {
+
+	public static void main(String[] args) {
+		
+		int spaces =0;
+	
+		for(int i=9;i>0;i--) {
+			
+			for(int j=1;j<=i;j++)
+				
+				System.out.print(j);
+			
+			for(int j=1;j<spaces;j++)
+				
+				System.out.print(" ");
+			for(int j=i;j>0;j--)
+			{
+				if(j!=9) {
+				System.out.print(j);
+			}
+				else if(j==9) {
+					j=8;
+					System.out.print(j);
+				}}
+			System.out.println();
+			spaces+=2;
+		}
+	}
+}
+
