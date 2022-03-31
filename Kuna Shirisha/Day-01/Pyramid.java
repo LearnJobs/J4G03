b6dd32ea02d9bb46ac09a83c2111a3e6cd895c24
KUNA-SHIRISHA <102514262+KUNA-SHIRISHA@users.noreply.github.com>
2022-03-31 16:20:28 +0530

    Add files via upload

---
+++ Kuna Shirisha/Day-01/Pyramid.java
@@ -0,0 +1,28 @@
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+	
+		int spaces = 40;
+		
+		int stars = 1;
+
+		for(int i=1; i<=10; i++)
+		{
+			for(int j=0; j<spaces; j++)
+				
+				System.out.print(" ");
+			
+			for(int j=0; j<stars; j++) 
+				
+				System.out.print("*");
+			
+			    System.out.println();
+			
+			spaces -= 1;
+			
+			stars += 2;
+		}
+	}
+
+}
