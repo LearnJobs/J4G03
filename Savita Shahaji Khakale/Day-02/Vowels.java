5a28aef682222279b8d2c0dbbfa58a3b3abda896
Savita Khakale <102578118+Savitakhakale@users.noreply.github.com>
2022-03-31 17:01:19 +0530

    Add files via upload

---
+++ Savita Shahaji Khakale/Day-02/Vowels.java
@@ -0,0 +1,22 @@
+package Day2;
+public class Vowels {
+
+	public static void main(String[] args) {
+		//String text = "Quick Brown Fox Jumps Over The Lazy Dog";
+		String text = "We are learning Java from LearnJobs by The Coders Nation";
+		char[] vowel = {'A','E','I','O','U','a','e','i','o','u'};
+
+		char c;
+		int count = 0;
+		for(int i=0; i<text.length(); i++) {
+			c = text.charAt(i);
+			for(int j=0; j<vowel.length; j++) {
+				if(c==vowel[j]) {
+					count++;
+				}
+			}
+		}
+		System.out.println("Totals Vowels = "+count);
+	}
+
+}
