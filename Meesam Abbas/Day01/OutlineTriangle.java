c0a211eec7553b3f02b39ffdfc232bb7ca7a87ba
meeabbas23 <82494386+meeabbas23@users.noreply.github.com>
2022-03-31 09:06:19 +0530

    Add files via upload

---
+++ Meesam Abbas/Day01/OutlineTriangle.java
@@ -0,0 +1,17 @@
+public class OutlineTriangle {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+			for(int j=1;j<=19;j++) {
+				if((i==10) || (i+j==11) || (i-j==9))
+				System.out.print("*");
+			
+			else {
+				System.out.print(" ");
+			}
+		}System.out.println();
+		}
+
+	}
+
+}
