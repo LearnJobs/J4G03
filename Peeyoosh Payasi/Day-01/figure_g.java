3c1b64013f7398ef9e2e8c969d19f3f33473575a
Peeyoosh22 <102529773+Peeyoosh22@users.noreply.github.com>
2022-04-01 15:32:09 +0530

    Add files via upload

---
+++ Peeyoosh Payasi/Day-01/figure_g.java
@@ -0,0 +1,25 @@
+
+public class figure_g {
+
+	public static void main(String[] args) {
+		int spaces = 14;
+		
+		for (int i=1;i<=9;i++) {
+			for(int j=1;j<=i;j++)System.out.print(j);
+			for (int j=0;j<=spaces;j++)System.out.print(" ");
+			for(int j=i;j>0;j--) {
+				if (j!=9) {
+					System.out.print(j);
+				}
+				else if(j==9) {
+					j=8;
+					System.out.print(j);
+				}
+			}
+			spaces-=2;
+			System.out.println();
+		}
+
+	}
+
+}
