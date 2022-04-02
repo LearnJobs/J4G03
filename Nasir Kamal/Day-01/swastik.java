60a8ee33684c82b816bfc14d5951f2391a8833c3
nasir97k <102578207+nasir97k@users.noreply.github.com>
2022-04-02 12:27:14 +0530

    Add files via upload

---
+++ Nasir Kamal/Day-01/swastik.java
@@ -0,0 +1,69 @@
+package swastik;
+
+public class swastik {
+	
+	
+
+		public static void main(String[] args)
+		{
+			String fill = "  ";
+			for (int i = 1; i <= 11; i++) {
+				if (i == 1) {
+					for (int j = 1; j <= 11; j++) {
+						if (j <= 6)
+							fill = "* ";
+						else if (j == 11)
+							fill = "* ";
+						else
+							fill = "  ";
+						System.out.print(fill);
+					}
+				} else if (i > 1 && i < 6) {
+					for (int j = 1; j <= 11; j++) {
+						if (j == 6)
+							fill = "* ";
+						else if (j == 11)
+							fill = "* ";
+						else
+							fill = "  ";
+						System.out.print(fill);
+					}
+				} else if (i == 6) {
+					fill = "* ";
+					for (int j = 1; j <= 11; j++) {
+						System.out.print(fill);
+					}
+				} else if (i > 6 && i < 11) {
+					for (int j = 1; j <= 11; j++) {
+						if (j == 1)
+							fill = "* ";
+						else if (j == 6)
+							fill = "* ";
+						else
+							fill = "  ";
+						System.out.print(fill);
+					}
+				} else if (i == 11) {
+					for (int j = 1; j <= 11; j++) {
+						if (j == 1)
+							fill = "* ";
+						else if (j >= 6)
+							fill = "* ";
+						else
+							fill = "  ";
+						System.out.print(fill);
+					}
+				}
+				System.out.println();
+			}
+
+
+		}
+
+	}
+
+	
+	
+	
+
+
