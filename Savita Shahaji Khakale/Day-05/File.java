ee3a50ca1b0203414672a284189e969f0f3d8784
Savita Khakale <102578118+Savitakhakale@users.noreply.github.com>
2022-04-02 17:12:52 +0530

    Add files via upload

---
+++ Savita Shahaji Khakale/Day-05/File.java
@@ -0,0 +1,38 @@
+package Day5;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+class File {
+	public String sourceFile = "textfile.txt";
+	public String destinationFile = "Copytextfile.txt";
+
+	public File() {
+		try {
+			InputStream in = new FileInputStream(sourceFile);
+			OutputStream out = new FileOutputStream(destinationFile);
+			
+			int i = 0;
+			while( (i=in.read()) != -1 ) {
+				out.write(i);
+			}
+			
+			out.close();
+			in.close();
+		} catch(FileNotFoundException e) {
+			System.out.println(e);
+		} catch(IOException e) {
+			System.out.println(e);
+		} catch(Exception e) {
+		}
+	}
+	
+	public static void main(String[] args) {
+		new File();
+	}
+
+}
