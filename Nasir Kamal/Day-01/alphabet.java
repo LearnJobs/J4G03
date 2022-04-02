6b51635ccb7f9d75d094c1f3018afe568bc62b91
nasir97k <102578207+nasir97k@users.noreply.github.com>
2022-04-02 11:04:21 +0530

    Add files via upload

---
+++ Nasir Kamal/Day-01/alphabet.java
@@ -0,0 +1,24 @@
+
+public class alphabet {
+
+		public static void main (String[] args)
+		{
+			for( int i = 0; i <= 5; i++ )
+			{
+				int alphabet = 65;
+				for( int j = 5; j > i; j-- ) 
+				{
+				System.out.print(" ");
+				
+				}
+				for( int k = 0; k <= i; k++ )
+					
+				{
+					System.out.print((char) (alphabet + k) + " ");
+						
+				}
+				System.out.println( );
+				
+			}
+		}
+}
