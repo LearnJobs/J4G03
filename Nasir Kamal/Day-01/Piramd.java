50be309793958e43404652f86f98d7918e784d80
nasir97k <102578207+nasir97k@users.noreply.github.com>
2022-04-02 10:01:10 +0530

    Add files via upload

---
+++ Nasir Kamal/Day-01/Piramd.java
@@ -0,0 +1,22 @@
+package Day01;
+
+public class Piramd {
+	
+	public static void main(String[] args) {
+		    int space = 50;
+		    int starts = 1;
+		    for(int i=1; i<=10; i++) {
+		        for(int j=0; j<space; j++) System.out.println(" ");
+		        for(int j=0; j<starts; j++)System.out.println("*");
+		   	    System.out.println();
+		    	space -=1;
+		    	starts +=2;
+		    }   
+		
+		}
+	}
+	
+	
+	
+
+
