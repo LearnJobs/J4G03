47681ba8f983c23794a026f2732bc66930425ba0
LearnJobs <101615128+LearnJobs@users.noreply.github.com>
2022-04-02 11:29:43 +0530

    Add files via upload

---
+++ Milan Das Class/Day-05/CopyFile.java
@@ -0,0 +1,43 @@
+package j4g03.day05;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/*
+ * 		To create another duplicate file.
+ * 		Create an identical file in another name
+ */
+
+public class CopyFile {
+	public String sourceFile = "textfile.txt";
+	public String destinationFile = "Copytextfile.txt";
+
+	public CopyFile() {
+		try {
+			InputStream in = new FileInputStream(sourceFile);
+			OutputStream out = new FileOutputStream(destinationFile);
+			
+			int i = 0;
+			while( (i=in.read()) != -1 ) {
+				out.write(i);
+			}
+			
+			out.close();
+			in.close();
+		} catch(FileNotFoundException e) {
+			System.out.println(e);
+		} catch(IOException e) {
+			System.out.println(e);
+		} catch(Exception e) {
+		}
+	}
+	
+	public static void main(String[] args) {
+		new CopyFile();
+	}
+
+}
---
+++ Milan Das Class/Day-05/IncompleteClass.java
@@ -0,0 +1,31 @@
+package j4g03.day05;
+
+public abstract class IncompleteClass {
+	public void flush() {
+		
+	}
+
+	public String split() {
+		return null;
+	}
+	
+	public abstract int read();
+}
+
+class ReadFromMemory extends IncompleteClass {
+	public int read() {
+		return 0;
+	}
+}
+
+class ReadFromHarddisk extends IncompleteClass {
+	public int read() {
+		return 0;
+	}
+}
+
+class ReadFromKeyboard extends IncompleteClass {
+	public int read() {
+		return 0;
+	}
+}
