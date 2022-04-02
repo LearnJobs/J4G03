410375ad9d291961bb6b695dfebda172527785c0
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-04-02 09:34:01 +0530

    Add files via upload

---
+++ Bolloju Satya Akil Kumar/daY-04/FileOutputStreamExample.java
@@ -0,0 +1,17 @@
+package begin4;
+
+import java.io.FileOutputStream;  
+
+public class FileOutputStreamExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 try{    
+             FileOutputStream fout=new FileOutputStream("D:\\testout.txt");    
+             fout.write(65);    
+             fout.close();    
+             System.out.println("success...");    
+            }catch(Exception e){System.out.println(e);}   
+	}
+
+}
