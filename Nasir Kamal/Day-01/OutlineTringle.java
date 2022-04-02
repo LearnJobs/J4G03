9b44b92677308c0bfe5de23958bc5899293684d7
nasir97k <102578207+nasir97k@users.noreply.github.com>
2022-04-02 11:07:27 +0530

    Add files via upload

---
+++ Nasir Kamal/Day-01/OutlineTringle.java
@@ -0,0 +1,22 @@
+
+public class OutlineTringle {
+	public static void main(String[] args) {
+			for(int i=1;i<=10;i++) {
+				for(int j=1;j<=19;j++) {
+					if((i==10) || (i+j==11) || (i-j==9))
+					System.out.print("*");
+				
+				else {
+					System.out.print(" ");
+				}
+			}System.out.println();
+			}
+
+		}
+
+	}
+	
+	
+	
+	
+
