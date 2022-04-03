d965430ef614f12ecf3ccdabd6bda780a9853e37
Ruturajpal <98735716+Ruturajpal@users.noreply.github.com>
2022-04-03 13:28:04 +0530

    Add files via upload

---
+++ Ruturaj Pal/day-05/CopyFile.java
@@ -0,0 +1,39 @@
+package com.razz1.day5;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class CopyFile {
+	public String sourceFile = "textfiles.txt";
+	public String destinationFile = "Copytextfiles.txt";
+
+	public CopyFile() {
+		try {
+			InputStream in = new FileInputStream(sourceFile);
+			OutputStream out = new FileOutputStream(destinationFile);
+			
+			int i = 0;
+			while( (i=in.read()) != -1 ) {
+				out.write(i);
+			}
+			
+			out.close();
+			in.close();
+		} catch(FileNotFoundException e) {
+			System.out.println(e);
+		} catch(IOException e) {
+			System.out.println(e);
+		} catch(Exception e) {
+		}
+	}
+	
+	public static void main(String[] args) {
+		new CopyText();
+	}
+}
+
+
