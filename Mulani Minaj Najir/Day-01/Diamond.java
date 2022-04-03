fbd64995dfb1155ca071dd1537a1575b54fdd71a
Shahnawaz1003 <100685075+Shahnawaz1003@users.noreply.github.com>
2022-04-03 15:53:20 +0530

    Add files via upload

---
+++ Mulani Minaj Najir/Day-01/Diamond.java
@@ -0,0 +1,46 @@
+package com.saif;
+
+public class Diamond {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i=1;i<=8;i++) 
+		{
+		    for(int j=8;j>=i;j--) 
+		    {
+		    	System.out.print(" ");
+		    	
+		    }	
+		    for(int k=1;k<=(2*i-1);k++)
+		    {
+		    
+		    	System.out.print("*");
+		    }
+			
+		    System.out.println();
+			
+		}
+
+		 
+		for(int i=8;i>=1;i--) 
+		{
+		    for(int j=8;j>=i;j--) 
+		    {
+		    	System.out.print(" ");
+		    	
+		    }	
+		    for(int k=1;k<=(2*i-1);k++)
+		    {
+		    
+		    	System.out.print("*");
+		    }
+			
+		    System.out.println();
+			
+		}
+		
+		
+	}
+
+}
