b5b10f95c12582907be335881e9831721f13db17
Jain1mini <102578212+Jain1mini@users.noreply.github.com>
2022-04-03 12:22:40 +0530

    Add files via upload

---
+++ Mini Jain/Day-04/fileoutputstream.java
@@ -0,0 +1,23 @@
+package begin.java;
+
+public class fileoutputstream {
+
+	public static void main(String[] args) {
+		public class FileOutputStream {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				 try{    
+		             FileOutputStream fout=new FileOutputStream("D:\\testout.txt");    
+		             fout.write(65);    
+		             fout.close();    
+		             System.out.println("success...");    
+		            }catch(Exception e){System.out.println(e);}   
+			}
+
+		}
+		
+
+	}
+
+}
