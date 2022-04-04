f81018176ce9fdfd3bb0eadc1116c1275e2afcbf
Abhradeep Bhattacharyya <31698461+Abhradeep2222@users.noreply.github.com>
2022-04-04 17:24:26 +0530

    Add files via upload

---
+++ Abhradeep Bhattacharyya/Day-04/IntroException2.java
@@ -0,0 +1,35 @@
+package j4g03.day04;
+
+public class IntroException2 {
+	public static void main(String[] args) {
+		int i = 1009;
+		int j = 0;
+		int k = 10;
+		String[] text = {"Red","Green","Blue","Magenta","Cyan","Yellow"};
+
+		for(int n=0; n<3; n++) {
+			try {
+				double r = i/j;
+				System.out.println(text[k]);
+				System.out.println(r);
+				System.out.println("There is no exception");
+			} catch(NumberFormatException e) {
+				System.out.println("NumberFormatException");
+			} catch(ArithmeticException | StringIndexOutOfBoundsException e) {
+				System.out.println("ArithmeticException");
+				j = 2;
+			} catch(ClassCastException | ArrayIndexOutOfBoundsException e) {
+				System.out.println("ClassCastException | ArrayIndexOutOfBoundsException");
+				k = 3;
+			} catch(Exception e) {
+				System.out.println("Exception");
+			} finally {
+				System.out.println("Finally block");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+
