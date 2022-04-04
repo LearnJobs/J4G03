4c1dcf90a10092f81108cfbbe52859c43bc91373
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-04-04 18:22:29 +0530

    Update College.java

--- Bolloju Satya Akil Kumar/day-05/College.java
+++ Bolloju Satya Akil Kumar/day-05/College.java
@@ -5,19 +5,19 @@ import java.io.Serializable;
 public class College implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	public String college_id;
+	public int college_id;
 	public String college_name;
 	public String college_location;
 
 
 	public College() {
-		college_id = "0";
+		college_id = 0;
 		college_name = "not told";
 		college_location = "Not told";		
 		
 	}
 
-	public College(String college_id, String college_name, String college_location) {
+	public College(int college_id, String college_name, String college_location) {
  
 		this.college_id = college_id;
 		this.college_name = college_name;
