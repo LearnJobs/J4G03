a101471979d2a636246e04ba15a11dab429b627f
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-04-04 18:23:43 +0530

    Update Library.java

--- Bolloju Satya Akil Kumar/day-05/Library.java
+++ Bolloju Satya Akil Kumar/day-05/Library.java
@@ -5,21 +5,21 @@ import java.io.Serializable;
 public class Library implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	public String lib_id;
+	public int lib_id;
 	public String lib_name;
 	public String lib_method;
 	public String  lib_location;
 
 
 	public Library() {
-		lib_id = "0";
+		lib_id = 0;
 		lib_name = "not told";
 		lib_method = "Not told";		
 		lib_location = "Not told";
 			
 	}
 
-	public Library(String lib_id, String lib_name, String lib_method, String lib_location) {
+	public Library(int lib_id, String lib_name, String lib_method, String lib_location) {
  
 		this.lib_id = lib_id;
 		this.lib_name = lib_name;
