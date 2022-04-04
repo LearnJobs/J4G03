0be60c9cccebdf69a98c7c78c509f5dad879b9c6
nasir97k <102578207+nasir97k@users.noreply.github.com>
2022-04-04 10:39:36 +0530

    Add files via upload

---
+++ Nasir Kamal/Day-4/Exception1.java
@@ -0,0 +1,33 @@
+package Day04;
+
+public class Exception1 {
+	public static void main(String[] args) {
+		int i = 1009;
+		int j = 2;
+		String[] text = {"Red","Green","Blue","Magenta","Cyan","Yellow"};
+		
+		try {
+			double r = i/j;
+			System.out.println(text[10]);
+			System.out.println(r);
+			System.out.println("There is no exception");
+		} catch(NumberFormatException e) {
+			System.out.println("NumberFormatException");
+		} catch(ArithmeticException | StringIndexOutOfBoundsException e) {
+			System.out.println("ArithmeticException");
+		} catch(ClassCastException | ArrayIndexOutOfBoundsException e) {
+			System.out.println("ClassCastException | ArrayIndexOutOfBoundsException");
+		} catch(Exception e) {
+			System.out.println("Exception");
+		} finally {
+			System.out.println("Finally block");
+		}
+	}
+	}
+	
+	
+	
+	
+	
+
+
