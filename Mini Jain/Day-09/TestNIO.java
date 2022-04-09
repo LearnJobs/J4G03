c73f47d70c8959e94379505d12afb9cecf53a052
Jain1mini <102578212+Jain1mini@users.noreply.github.com>
2022-04-09 10:16:24 +0530

    Add files via upload

---
+++ Mini Jain/Day-09/TestNIO.java
@@ -0,0 +1,46 @@
+
+public class TestNIO {
+	import java.io.IOException;
+	import java.io.RandomAccessFile;
+	import java.nio.ByteBuffer;
+	import java.nio.channels.FileChannel;
+
+	public class TestNIO {
+		RandomAccessFile raf;
+		FileChannel fc;
+
+		public TestNIO() {
+			try {
+				raf = new RandomAccessFile("textfile.txt","rw");
+				fc = raf.getChannel();
+
+				ByteBuffer bBuf = ByteBuffer.allocate(48);
+
+				int bRead = fc.read(bBuf);
+				while(bRead != -1) {
+					System.out.println("Read: "+bRead);
+					bBuf.flip();
+
+					while(bBuf.hasRemaining()) {
+						System.out.print((char)bBuf.get());
+					}
+
+					bBuf.clear();
+					bRead = fc.read(bBuf);
+				}
+
+			} catch(IOException e) {
+			} finally {
+				try {
+					fc.close();
+					raf.close();
+				} catch(IOException e1) {
+				}
+			}
+		}
+		
+		public static void main(String[] args) {
+			new TestNIO();
+		}
+	}
+}
