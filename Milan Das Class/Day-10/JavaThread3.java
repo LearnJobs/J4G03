72f4df16cb7c3de96be24554debf0bf21e62d032
LearnJobs <101615128+LearnJobs@users.noreply.github.com>
2022-04-09 12:58:05 +0530

    Add files via upload

--- Milan Das Class/Day-10/JavaThread3.java
+++ Milan Das Class/Day-10/JavaThread3.java
@@ -19,3 +19,60 @@ public class JavaThread3 implements Runnable {
 		}
 	}
 }
+
+/*
+ * Sample Output:
+	Loop-1: Z  
+	Loop-1: Y  
+	Loop-1: X  
+	Loop-1: W  
+	Loop-2: z  
+	Loop-2: y  
+	Loop-2: x  
+	Loop-2: w  
+	Loop-2: v  
+	Loop-2: u  
+	Loop-2: t  
+	Loop-2: s  
+	Loop-2: r  
+	Loop-2: q  
+	Loop-2: p  
+	Loop-2: o  
+	Loop-2: n  
+	Loop-2: m  
+	Loop-1: V  
+	Loop-1: U  
+	Loop-1: T  
+	Loop-1: S  
+	Loop-1: R  
+	Loop-1: Q  
+	Loop-1: P  
+	Loop-1: O  
+	Loop-1: N  
+	Loop-2: l  
+	Loop-2: k  
+	Loop-2: j  
+	Loop-2: i  
+	Loop-2: h  
+	Loop-2: g  
+	Loop-2: f  
+	Loop-1: M  
+	Loop-1: L  
+	Loop-1: K  
+	Loop-1: J  
+	Loop-1: I  
+	Loop-2: e  
+	Loop-2: d  
+	Loop-2: c  
+	Loop-2: b  
+	Loop-2: a  
+	Loop-1: H  
+	Loop-1: G  
+	Loop-1: F  
+	Loop-1: E  
+	Loop-1: D  
+	Loop-1: C  
+	Loop-1: B  
+	Loop-1: A  
+*/
+
