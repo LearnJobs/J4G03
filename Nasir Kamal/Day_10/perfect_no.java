0b1ee1c809549b02d4f33589143088e925066b53
nasir97k <102578207+nasir97k@users.noreply.github.com>
2022-04-09 10:53:53 +0530

    Add files via upload

---
+++ Nasir Kamal/Day_10/perfect_no.java
@@ -0,0 +1,49 @@
+package Day_10;
+
+import java.util.Scanner;
+
+public class perfect_no {
+	
+	public static void main(String args[])    
+	{  
+	long n, sum=0;  
+	Scanner sc=new Scanner(System.in);         
+	System.out.print("Enter the number: ");  
+	n=sc.nextLong();  
+	int i=1;  
+	  
+	while(i <= n/2)  
+	{  
+	if(n % i == 0)  
+	{  
+	  
+	sum = sum + i;  
+	}   
+	  
+	i++;  
+	}   
+	if(sum==n)  
+	{  
+	  
+	System.out.println(n+" is a perfect number.");  
+	}   
+	else  
+	  
+	System.out.println(n+" is not a perfect number.");   
+	}  
+	}  
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
