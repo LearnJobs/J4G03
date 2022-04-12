d73445f0b64dfa13938f4047833a7c62a49cec39
Yadavsona <102577991+Yadavsona@users.noreply.github.com>
2022-04-12 16:22:25 +0530

    Add files via upload

---
+++ Sonali Baburao Yadav/Day-4/Election_Ages.java
@@ -0,0 +1,21 @@
+
+public class Election_Ages {
+      int age;
+       String ages[]= {"25","35","11","19","55","94","XY","75","-3","22","12","10","45","51","13"};
+       public ElectionAgeCheck2() {
+    	   for(int i=0;i<ages.length;i++) {
+    		   try {
+    			   age=Integer.parseInt(ages[i]);
+    			   doAgeCheck(age);
+    			   
+    		   }
+    	   }
+       
+	public static void main(String[] args) {
+		
+			
+		}
+
+	}
+
+}
