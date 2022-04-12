8baca1f82c80543189f142640509a13e0a314f3a
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-04-12 14:48:20 +0530

    Add files via upload

---
+++ Bolloju Satya Akil Kumar/Fibonacci.java
@@ -0,0 +1,23 @@
+package begin;
+
+public class Fibonacci {
+	int count = 0;
+	public Fibonacci() {
+		int f1 = 0;
+		int f2 = 1;
+		System.out.print(f1 + " ");
+		next(f1,f2);
+	}
+
+	public void next(int f1, int f2) {
+		System.out.print(f2 + " ");
+		f1 = f1+f2;
+		if(++count>20) return;
+		next(f2,f1);
+	}
+	
+	public static void main(String[] args) {
+		new Fibonacci();
+	}
+
+}
