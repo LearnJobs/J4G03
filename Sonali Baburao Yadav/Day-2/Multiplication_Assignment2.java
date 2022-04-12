8b25b705eb10f17694883bdebebe2239ffc4b6a5
Yadavsona <102577991+Yadavsona@users.noreply.github.com>
2022-04-12 16:18:46 +0530

    Add files via upload

---
+++ Sonali Baburao Yadav/Day-2/Multiplication_Assignment2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Multiplication_Assignment2 {
+
+	public Multiplication_Assignment2(int begining, int ending) {
+		for(int i=1; i<=10; i++) {
+			for(int j=begining; j<=ending; j++) {
+				System.out.print(padLeft(j,2) + " x " + padLeft(i,2) + " = " + padLeft((i*j),3) + "   ");
+			}
+			System.out.println();}}
+    public String padLeft(int number, int length) {
+		StringBuffer buf = new StringBuffer(String.valueOf(number));
+		for(int i=buf.length(); i<length; i++) {
+			buf.insert(0," ");    
+		}
+		return buf.toString();}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter Begining point: ");
+		int start= sc.nextInt();
+		System.out.print("enter ending point: ");
+		int end = sc.nextInt();
+		if(start<end) {
+			int temp_end=start+4;
+			while(temp_end<=end) {
+				new Multiplication_Assignment2(start, temp_end);
+				start+=5;
+				temp_end+=5;
+				System.out.println("\n");
+			}
+			int remain = temp_end-end;
+			temp_end-=remain;
+			new Multiplication_Assignment2(start, temp_end);
+			System.out.println();
+		}
+		else {
+			System.err.println("begining is always less");
+		}
+	}
+}
