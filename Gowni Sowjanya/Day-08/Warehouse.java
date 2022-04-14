857e9a4b9c5da7cdc648493c4efa59590b3c2719
Gowni Sowjanya <102508202+Gownisowjanya@users.noreply.github.com>
2022-04-14 14:58:34 +0530

    Add files via upload

---
+++ Gowni Sowjanya/Day-08/Warehouse.java
@@ -0,0 +1,42 @@
+
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Warehouse {
+	public Connection conn;
+	public Statement stmt;
+	public ResultSet rs;
+	public StringBuffer buf;
+	
+	public Warehouse() {
+		try {
+			Class.forName("org.gjt.mm.mysql.Driver");
+			
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/WAREHOUSE1","root","Sowjanya123");
+			
+			stmt = conn.createStatement();
+			
+			rs = stmt.executeQuery("select * from warehouse");
+			
+			while(rs.next()) {
+				buf = new StringBuffer();
+				buf.append(rs.getString("warehouse_name"));
+				System.out.println(buf);
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println(e);
+		}catch (SQLException e) {
+			System.out.println(e);
+		}finally {
+			try {
+				conn.close();
+			}catch (SQLException e) {
+				System.out.println(e);
+			}
+		}
+	}
+}
