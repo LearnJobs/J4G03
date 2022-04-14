1a43e80afd5daddd5379379ad5afb7a47393acf7
Soundaryadt <103351580+Soundaryadt@users.noreply.github.com>
2022-04-14 11:39:50 +0530

    Add files via upload

---
+++ Soundarya Tamtam/Day3/everyday.java
@@ -0,0 +1,21 @@
+package learnjob;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+
+
+public class everyday {
+	public static void main (String [] args) {
+	
+		LocalDate today= LocalDate.of(2022, Month.MARCH, 31);
+		
+		LocalDate sunday= LocalDate.of(2022, Month.APRIL, 3);
+		
+		long numberofdays =ChronoUnit.DAYS.between(today,sunday);
+		System.out.println(numberofdays);
+		
+	}
+
+}
