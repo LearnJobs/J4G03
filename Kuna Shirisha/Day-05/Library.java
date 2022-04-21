ca4856cf903e6f8ceb999306bf0d9d031b7f34bd
KUNA-SHIRISHA <102514262+KUNA-SHIRISHA@users.noreply.github.com>
2022-04-21 12:57:17 +0530

    Add files via upload

---
+++ Kuna Shirisha/Day-05/Library.java
@@ -0,0 +1,30 @@
+package DAY5;
+
+import java.io.Serializable;
+
+public class Library implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public int lib_id;
+	public String lib_name;
+	public String lib_method;
+	public String  lib_location;
+
+
+	public Library() {
+		lib_id = 0;
+		lib_name = "not told";
+		lib_method = "Not told";		
+		lib_location = "Not told";
+			
+	}
+
+	public Library(int lib_id, String lib_name, String lib_method, String lib_location) {
+ 
+		this.lib_id = lib_id;
+		this.lib_name = lib_name;
+		this.lib_method = lib_method;
+		this.lib_location = lib_location;
+	
+	}
+}	
---
+++ Kuna Shirisha/Day-05/LibrarySerializeIn.java
@@ -0,0 +1,43 @@
+package DAY5;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.List;
+// Deserialization
+public class LibrarySerializeIn {
+
+	public static void main(String[] args) {
+		try {
+			FileInputStream fis = new FileInputStream("LibraryObject.ser");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+
+			List<Library> list = (List<Library>)ois.readObject();
+			Library c1 = list.get(0);
+			Library c2 = list.get(1);
+			Library c3 = list.get(2);
+			Library c4 = list.get(3);
+
+			ois.close();
+			fis.close();
+
+		
+			System.out.println(c1.lib_id);
+			System.out.println(c1.lib_name);
+			System.out.println(c1.lib_method);
+			System.out.println(c1.lib_location);
+		
+			
+		
+
+		} catch(FileNotFoundException e) {
+			System.out.println(e);
+		} catch(IOException e) {
+			System.out.println(e);
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
