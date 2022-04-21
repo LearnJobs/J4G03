230babfb8df17a903b9dab5bdf9e6f764cc3b46a
pratimapawar <102610287+pratimapawar@users.noreply.github.com>
2022-04-21 18:13:12 +0530

    Add files via upload

---
+++ Pratima Abhijit Patil/Day-1/PrimeNo.java
@@ -0,0 +1,14 @@
+public class PrimeNo {
+	public static void main(String[] args) {
+		int n = 1009;
+		boolean yn = true;
+		for(int j=2; j<n; j++) {
+			if(n%j==0) {
+				yn = false;
+				break;
+			}
+		}
+		if(yn) System.out.println(n+" is a prime number");
+		else   System.out.println(n+" is not a prime number");
+	}
+}
