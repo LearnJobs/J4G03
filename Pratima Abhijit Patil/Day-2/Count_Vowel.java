d5363e360802c2cf1cbd5aa4c7e99c7b97c1dc94
pratimapawar <102610287+pratimapawar@users.noreply.github.com>
2022-04-21 23:14:11 +0530

    Add files via upload

---
+++ Pratima Abhijit Patil/Day-2/Count_Vowel.java
@@ -0,0 +1,23 @@
+public class Count_Vowel {
+
+	public static void main(String[] args) {
+		String text="we arevlearningjava learnjobs by the code nation";
+		char[] vowel= {'A','E','I','O','U','a','e','i','o','u'};
+		char c;
+		int count=0;
+		for(int i=0; i<text.length();i++) {
+			c=text.charAt(i);
+			for(int j=0;j<vowel.length;j++) {
+				if(c==vowel[j]) {
+					count++;
+				}
+			}
+		}
+System.out.println("totalvowels="+count);
+	}
+
+
+
+	}
+
+
