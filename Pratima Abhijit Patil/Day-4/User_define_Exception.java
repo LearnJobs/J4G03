1e77f95595f0881f70c351680ea7fbaf118a7d2b
pratimapawar <102610287+pratimapawar@users.noreply.github.com>
2022-04-21 23:51:19 +0530

    Add files via upload

---
+++ Pratima Abhijit Patil/Day-4/User_define_Exception.java
@@ -0,0 +1,14 @@
+import java.rmi.NotBoundException;
+
+public class User_define_Exception {
+
+	public static void main(String[] args) throws NotBoundException {
+		try {System.out.println("User Defined Exception");
+		}catch(ArrayIndexOutOfBoundsException e) {
+		}catch(StringIndexOutOfBoundsException e) {
+		}catch(MyException e) {
+		}
+	}
+}
+class MyException extends RuntimeException{
+}
