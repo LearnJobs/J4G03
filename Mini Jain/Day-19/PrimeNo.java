fa360bea700acdb8998495c3663476c6c2b622b3
Mini jain <102578212+Jain1mini@users.noreply.github.com>
2022-04-22 20:29:47 +0530

    Add files via upload

---
+++ Mini Jain/Day-19/PrimeNo.java
@@ -0,0 +1,43 @@
+package day19.java;
+
+
+
+	import java.io.IOException;
+	import java.io.PrintWriter;
+
+	import javax.servlet.http.HttpServlet;
+	import javax.servlet.http.HttpServletRequest;
+	import javax.servlet.http.HttpServletResponse;
+
+	public class PrimeNo extends HttpServlet {
+		public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+			PrintWriter out = res.getWriter();
+			int count = 0;
+			boolean yn;
+			
+			out.println("<table border='1' cellpadding='5'>");
+			out.println("<tr>");
+			for(int i=1000; i<=2000; i++) {
+				yn = true;
+				for(int j=2; j<i; j++) {
+					if(i%j==0) {
+						yn = false;
+						break;
+					}
+				}
+				if(yn) {
+					out.println("<td>"+i+"</td>");
+					count++;
+					if(count>=15) {
+						out.println("</tr>");
+						out.println("<tr>");
+						count = 0;
+					}
+				}
+			}
+			out.println("</tr>");
+			out.println("</table>");
+		}
+	}
+
+
