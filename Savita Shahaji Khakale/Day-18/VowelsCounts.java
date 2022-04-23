bc6c3d2fba8d1d0d5df0529a1c624d7a9f436815
Savita Khakale <102578118+Savitakhakale@users.noreply.github.com>
2022-04-23 09:56:32 +0530

    Add files via upload

---
+++ Savita Shahaji Khakale/Day-18/VowelsCounts.java
@@ -0,0 +1,29 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class VowelsCounts extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter o = res.getWriter();
+		String text2count = req.getParameter("str2count");
+		String vowels = "AEIOUaeiou";
+		int count = 0;
+		char c;
+
+		for(int i=0; i<text2count.length(); i++) {
+			c = text2count.charAt(i);
+			for(int j=0; j<vowels.length(); j++) {
+				if(c==vowels.charAt(j)) {
+					count++;
+					break;
+				}
+			}
+		}
+		o.println("<h1>Vowel Count for \"" + text2count + "\" is " + count + "</h2><br><br>");
+	}
+}
