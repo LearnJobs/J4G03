5fbe551764ddb44a4abb1903505639bc52a49fcd
LearnJobs <101615128+LearnJobs@users.noreply.github.com>
2022-04-25 12:51:25 +0530

    Add files via upload

---
+++ Milan Das Class/Day-21/AddServlet.java
@@ -0,0 +1,24 @@
+package com.learnjobs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int start = Integer.parseInt(req.getParameter("first"));
+		int second = Integer.parseInt(req.getParameter("second"));
+		int sum = start + second;
+		
+		PrintWriter out = res.getWriter();
+		out.println("The first number is " + start + "<br>");
+		out.println("The second number is " + second + "<br>");
+		out.println("The SUM is " + sum + "<br>");
+		out.println("<br>");
+		out.println("<a href='multiply?first="+start+"&second="+second+"'>Multiply</a>");
+		out.println("<br>");
+	}
+}
---
+++ Milan Das Class/Day-21/LoginServlet.java
@@ -0,0 +1,38 @@
+package com.learnjobs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		String username = req.getParameter("user");
+		String password = req.getParameter("password");
+
+		PrintWriter out = res.getWriter();
+		
+		out.println("<h2>" + username + "</h2>");
+		out.println("<h2>" + password + "</h2>");
+		
+		/* ----------------------------------------------------------- */
+		
+		Enumeration<String> pNames = req.getParameterNames();
+		String parameter;
+		String value;
+		out.println("<table border='1' cellspacing='5'>");
+		while(pNames.hasMoreElements()) {
+			parameter = pNames.nextElement();
+			value = req.getParameter(parameter);
+			out.println("<tr>");
+			out.println("<td>"+ parameter + "</td>");
+			out.println("<td>" + value + "</td>");
+			out.println("</tr>");
+		}
+		out.println("</table>");
+		out.println("<br><br>");
+	}
+}
