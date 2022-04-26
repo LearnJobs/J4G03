435529dbfb50df311dc387033b87ab768e6e8b19
Savita Khakale <102578118+Savitakhakale@users.noreply.github.com>
2022-04-26 09:31:34 +0530

    Add files via upload

---
+++ Savita Shahaji Khakale/Day-20/MultiplyServlet.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MultiplyServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int n1 = Integer.parseInt(req.getParameter("first"));
+		int n2 = Integer.parseInt(req.getParameter("second"));
+		int prod = n1 * n2;
+		res.getWriter().println("<h2>Product of the 2 numbers is "+prod+"</h2>");
+
+		res.getWriter().println("This is Mutiplication Servlet");
+	}
+}
