180c66d765cf2f3f7a5af08a65e80670ed7bdf3f
Mini jain <102578212+Jain1mini@users.noreply.github.com>
2022-04-26 20:22:22 +0530

    Add files via upload

---
+++ Mini Jain/Day-21/Add2Servlet.java
@@ -0,0 +1,20 @@
+package com.mini;
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@SuppressWarnings("serial")
+public class Add2Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		RequestDispatcher rd = req.getRequestDispatcher("multiply");
+		rd.forward(req, res);
+	}
+}
