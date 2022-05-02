70bd66c64a10d83f872bb408685cfbfd1307f16b
Savita Khakale <102578118+Savitakhakale@users.noreply.github.com>
2022-05-02 10:19:11 +0530

    Add files via upload

---
+++ Savita Shahaji Khakale/Day-23/Palindrome.ts
@@ -0,0 +1,14 @@
+function palindrome(str)
+{
+     var len = str.length;
+     var mid = Math.floor(len/2);
+
+     for(var i=0; i<mid; i++)
+     {
+          if(str[i] !==str[len - i])
+          {
+              return false;
+          }
+          return true;
+     }
+}
