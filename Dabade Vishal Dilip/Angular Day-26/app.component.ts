f55cb35eb167f52ece3e82dc88e7fd4d29f5d47b
Dabade Vishal <102508123+VishalDabade@users.noreply.github.com>
2022-05-04 13:33:50 +0530

    Add files via upload

---
+++ Dabade Vishal Dilip/Angular Day-26/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl:'./app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+
+     color=['red','black','pink','yellow','green','white','blue','cyan','brown','orange'];
+     DataHideSystem:boolean=true;
+
+}
