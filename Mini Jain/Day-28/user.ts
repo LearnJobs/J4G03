8c358023fd6f09e6476ba46fae81d069ae6372a0
Mini jain <102578212+Jain1mini@users.noreply.github.com>
2022-05-05 20:10:55 +0530

    Add files via upload

---
+++ Mini Jain/Day-28/user.ts
@@ -0,0 +1,14 @@
+export class User {
+    constructor(
+        public firstname: string,
+        public lastname: string,
+        public fathername: string,
+        public gender: string,
+        public dob: string,
+        public Qualification: string,
+        public email: string,
+        public phone: string,
+        public Apply: string,
+        public address: string
+    ) {}
+}
