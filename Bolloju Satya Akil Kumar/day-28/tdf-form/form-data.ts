c10d6ba4161bc06a09186c00a3d846fb4fe5ad2a
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-05-05 13:06:33 +0530

    Add files via upload

---
+++ Bolloju Satya Akil Kumar/day-28/tdf-form/form-data.ts
@@ -0,0 +1,10 @@
+export class FormData {
+    constructor(
+        public name: string,
+        public email: string,
+        public phone: string,
+        public topic: string,
+        public time: string,
+        public subscribe: boolean
+    ) {}
+}
