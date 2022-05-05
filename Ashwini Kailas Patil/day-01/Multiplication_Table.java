86e265ef9ca67d7adeaa5a82c8f806eb2b2fd2ac
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-05-05 09:42:03 +0530

    Add files via upload

---
+++ Ashwini Kailas Patil/day-01/Multiplication_Table.java
@@ -0,0 +1,17 @@
+package begin;
+
+import java.util.Scanner;
+public class Multiplication_Table 
+{
+    public static void main(String[] args) 
+    {
+        try (Scanner s = new Scanner(System.in)) {
+			System.out.print("Enter number:");        
+			int n=s.nextInt();
+			    for(int i=1; i <= 10; i++)
+			    {
+			        System.out.println(n+" * "+i+" = "+n*i);
+			    }
+		}
+    }
+}
---
+++ Ashwini Kailas Patil/day-01/PalindromeTriangle.java
@@ -0,0 +1,16 @@
+package begin;
+
+public class PalindromeTriangle {
+
+	public static void main(String[] args) {
+		int spaces =50;
+		
+		for(int i =1;i<=9;i++) {
+			for(int j=0;j<spaces;j++)System.out.print(" ");
+			for(int j=1;j<=i;j++) System.out.print(j);
+			for(int j=i-1;j>0;j--)System.out.print(j);
+			System.out.println();
+			spaces --;	
+		}
+	}
+}
