55ffd18b3ade1b5a710a7af0e436e108c562da10
ashuvarshney111 <102507082+ashuvarshney111@users.noreply.github.com>
2022-05-07 00:53:53 +0530

    Create form-data.ts

---
+++ Ashu Varshney/Day-29/form-data.ts
@@ -0,0 +1,8 @@
+export class FormData {
+    constructor(
+          public name: string,
+          public pass: string,
+          public rempsw: string              
+    ){}
+}
+© 2022 GitHub, Inc.
