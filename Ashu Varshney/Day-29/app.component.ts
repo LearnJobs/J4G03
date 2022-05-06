7bc4228dbec6c0e047614a5e385a133c51fd65e6
ashuvarshney111 <102507082+ashuvarshney111@users.noreply.github.com>
2022-05-07 00:53:07 +0530

    Create app.component.ts

---
+++ Ashu Varshney/Day-29/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormData } from './form-data';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+public userModel = new FormData("ashu","ashu@111","ashu@111");
+}
+© 2022 GitHub, Inc.
