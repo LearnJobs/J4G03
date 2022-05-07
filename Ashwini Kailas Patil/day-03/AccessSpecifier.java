81d21c4b229306084d93fc8a8734fbcf8e9b349f
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-05-07 14:57:07 +0530

    Add files via upload

---
+++ Ashwini Kailas Patil/day-03/AccessSpecifier.java
@@ -0,0 +1,13 @@
+package begin3;
+
+public class AccessSpecifier {
+
+	static {
+		System.out.println("This line is defined in the static block");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("This line is defined in the main method");
+	}
+
+}
