6d4c32ce9a523a7bc525a9040667db0fa06dcea5
Savita Khakale <102578118+Savitakhakale@users.noreply.github.com>
2022-05-07 10:12:24 +0530

    Add files via upload

---
+++ Savita Shahaji Khakale/Day-27/Jio.java
@@ -0,0 +1,17 @@
+package com.tcn.ioc;
+
+public class Jio implements Sim{
+
+		public void Dialing() {
+			  System.out.println("Talking Jio ISP");
+			  System.out.println("Jio working..");
+		}
+		
+		public void Talk() {
+			  System.out.println("Talking Jio ISP");
+			  System.out.println("Jio working..");
+
+	}
+
+}
+
---
+++ Savita Shahaji Khakale/Day-27/Vodafone.java
@@ -0,0 +1,16 @@
+package com.tcn.ioc;
+
+public class Vodafone implements Sim {
+
+
+	public void Dialing() {
+		  System.out.println("Talking Vodafone ISP");
+		  System.out.println("Vodafone working..");
+	}
+	
+	public void Talk() {
+		  System.out.println("Talking Vodafone ISP");
+		  System.out.println("Vodafone working..");
+
+}
+}
