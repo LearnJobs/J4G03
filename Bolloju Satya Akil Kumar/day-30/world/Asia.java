1059b5644396fcef333f8b6f66154bfdc660b1ab
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-05-09 15:54:57 +0530

    Add files via upload

---
+++ Bolloju Satya Akil Kumar/day-30/world/Asia.java
@@ -0,0 +1,19 @@
+package com.world.autowire;
+
+public class Asia {
+
+	private String  noOfCountries;
+
+	public String getNoOfCountries() {
+		return noOfCountries;
+	}
+
+	public void setNoOfCountries(String noOfCountries) {
+		this.noOfCountries = noOfCountries;
+	}
+	
+	 public void noOfCountries() {
+		 System.out.println("noOfCountries in the Asia are:48");
+    }
+
+}
