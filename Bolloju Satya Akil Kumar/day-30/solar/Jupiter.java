99e0133b8d8bcd3d8ece1fbe7687cbd6d4877761
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-05-09 15:53:45 +0530

    Add files via upload

---
+++ Bolloju Satya Akil Kumar/day-30/solar/Jupiter.java
@@ -0,0 +1,26 @@
+package com.solarsystem.autowire;
+
+public class Jupiter {
+
+	private String diameter;
+    private String surfaceArea;  
+	
+	  public String getDiameter() {
+		return diameter;
+	}
+	public void setDiameter(String diameter) {
+		this.diameter = diameter;
+	}
+	public String getsurfaceArea() {
+		return surfaceArea;
+	}
+	public void setsurfaceArea(String surfaceArea) {
+		this.surfaceArea = surfaceArea;
+	}
+	public void diameter() {
+		System.out.println("diameter of the Jupiter planet:143,000 km");
+	}
+	public void surfaceArea() {
+		System.out.println("surfaceArea of the Jupiter planet:61.42 billion km²");
+	}
+}
---
+++ Bolloju Satya Akil Kumar/day-30/solar/Sun.java
@@ -0,0 +1,26 @@
+package com.solarsystem.autowire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Sun {
+
+
+	public static void main(String[] args) {
+		//SolarSystem solarsystem = new SolarSystem();
+		//solarsystem.diameter();
+		//solarsystem.surfaceArea();
+
+
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+	
+		SolarSystem solarsystem= context.getBean("solarsystem", SolarSystem.class);
+		
+		solarsystem.showdata();
+	
+
+	}
+
+}
