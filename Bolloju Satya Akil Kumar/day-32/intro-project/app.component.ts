b209a94491b9d6d520753924e0920a328e68f454
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-05-12 15:51:33 +0530

    Add files via upload

---
+++ Bolloju Satya Akil Kumar/day-32/intro-project/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+      <!-- Interpolation -->
+      <h1>This is my first project</h1>
+      <h2>{{batches}}</h2>
+      <h2>{{name.toUpperCase()}}</h2>
+      <h2>{{25 + 35}}</h2>
+      <h2>{{"Hello " + "World!"}}</h2>
+      <h2>{{greet()}}</h2>
+
+      <hr>
+      <!-- Property binding -->
+      <input type="text" value="LearnJobs" [id]="myid" /><br>
+      <input type="text" value="Herculus" id="{{myid2}}" /><br>
+
+      <hr>
+      <!-- Class binding -->
+      <h2 class="success">Are you correct?</h2>
+      <h2 [class]="errorClass">Are you correct?</h2>
+      <h2 [class]="successClass">Are you correct?-3</h2>
+      <h2 [class]="specialClass">Are you correct?</h2>
+      <h2 [class.error]="isError">Are you correct?-5</h2>
+
+      <hr>
+      <!--Style binding-->
+      <h2 [style.color]="'blue'">This is style Binding</h2>
+      <h2 [style.color]="false ? 'green' : 'red'">This is style Binding</h2>
+      <h2 [style.color]="isError ? 'green' : 'red'">This is style Binding</h2>
+      <h2 [style.color]="isError ? 'green' : 'red'">This is style Binding</h2>
+      <h2 [style.color]="isError ? 'green' : 'red'">Quick fox jumps over the dog</h2>
+      `,
+  styles: ['h1 {background-color:pink; color:red; text-align: right;}','.success {color:green;}', '.error {color:red;}','.special {font-style:italic;}']
+  //templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
+ 
+})
+export class AppComponent implements OnInit {
+    public batches:string = "FSD-02 + GAVS-02 + GAVS-03";
+    public name:string = "The Coders Nation";
+    myid = "book";
+    myid2 = "notebook";
+    classname = "alignment";
+    isError = true
+    errorClass = "error";
+    successClass = "success";
+    specialClass = "special";
+
+    ngOnInit() {
+    }
+
+    public greet():string {
+        return "Welcome " + this.name;
+    }
+}
