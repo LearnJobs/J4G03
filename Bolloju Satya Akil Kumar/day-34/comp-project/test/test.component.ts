099487f57e469d6fcbf0b70e840ebffac4e74a30
akhilkumar35 <102572793+akhilkumar35@users.noreply.github.com>
2022-05-13 16:33:17 +0530

    Add files via upload

---
+++ Bolloju Satya Akil Kumar/day-34/comp-project/test/test.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  @Input("parentData") public ms_ceo: any;
+  @Output() public childEvent = new EventEmitter();
+  public company = "ThE CoDeRs NaTiOn";
+  public leader = {
+      name: "Narendra Modi",
+      age: 68,
+      designation: "Prime Minister",
+      location: "New Delhi"
+  };
+  public today = new Date();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  fireEvent() {
+      this.childEvent.emit("Hello Milan Das");
+  }
+
+}
