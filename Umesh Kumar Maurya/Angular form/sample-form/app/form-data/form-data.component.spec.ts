2b9c67146cc02028d7658a0ad7a4e74c8f0287d9
Umesh Kumar Maurya <61643296+Umeshkmaurya197@users.noreply.github.com>
2022-05-18 09:45:00 +0530

    Add files via upload

---
+++ Umesh Kumar Maurya/Angular form/sample-form/app/form-data/form-data.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormDataComponent } from './form-data.component';
+
+describe('FormDataComponent', () => {
+  let component: FormDataComponent;
+  let fixture: ComponentFixture<FormDataComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FormDataComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormDataComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Umesh Kumar Maurya/Angular form/sample-form/app/form-data/form-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-data',
+  templateUrl: './form-data.component.html',
+  styleUrls: ['./form-data.component.css']
+})
+export class FormDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
